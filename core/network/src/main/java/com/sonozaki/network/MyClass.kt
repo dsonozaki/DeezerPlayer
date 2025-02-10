1ee8235892ca8ed04ceab2cb9753a98553caddc1
sonozaki <sonozaki@sonozaki.sonozaki>
2025-02-10 15:36:29 +0300

    Initial modularization

---
+++ core/network/src/main/java/com/sonozaki/network/MyClass.kt
@@ -0,0 +1,4 @@
+package com.sonozaki.network
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Deezer Player"
+include(":app")
+include(":core:resources")
+include(":core:network")
+include(":core:presentation")
+include(":features:player")
+include(":features:localtracks")
+include(":features:deezertracks")
+include(":data:localtracks")
+include(":data:deezertracks")
